00e952860a76c88e76b48a3a4bcb02744c2d97ad
BVeronezi <bds.veronezi@gmail.com>
2022-01-21 15:37:26 -0300

    config env

--- next.config.js
+++ next.config.js
@@ -1,20 +1,7 @@
-const { parsed: localEnv } = require("dotenv").config();
-const webpack = require("webpack");
-
 module.exports = {
   reactStrictMode: true,
-  webpack(config) {
-    config.plugins.push(new webpack.EnvironmentPlugin(localEnv));
-    config.node = { fs: "empty" };
-    config.plugins = config.plugins || [];
-
-    config.plugins = [...config.plugins];
-
-    return config;
-  },
   env: {
-    NEXT_PUBLIC_API_ENDPOINT: "https://meiup-api.herokuapp.com/api/v1",
-    NEXT_PUBLIC_KEY_CEP:
-      "d7756953-d64d-46a3-8a7f-ffb409dd20a0-38e52cca-6626-41e9-950b-f69496b95a0a",
+    NEXT_PUBLIC_API_ENDPOINT: process.env.NEXT_PUBLIC_API_ENDPOINT,
+    NEXT_PUBLIC_KEY_CEP: process.env.NEXT_PUBLIC_KEY_CEP,
   },
 };
