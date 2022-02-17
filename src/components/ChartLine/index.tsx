772f67075147179fb8b489b91c72fa7e43d31abf
BVeronezi <bds.veronezi@gmail.com>
2022-02-17 20:05:21 -0300

    dashboard

--- src/components/ChartLine/index.tsx
+++ src/components/ChartLine/index.tsx
@@ -1,66 +1,60 @@
-import { theme} from "@chakra-ui/react";
+import { theme } from "@chakra-ui/react";
 import dynamic from "next/dynamic";
+import moment from "moment";
 
-const Chart = dynamic(() => import('react-apexcharts'), {
-    ssr: false,
+const Chart = dynamic(() => import("react-apexcharts"), {
+  ssr: false,
 });
 
-const options: ApexCharts.ApexOptions = {
+export function ChartLineDashboard({ vendas }) {
+  const options: ApexCharts.ApexOptions = {
     chart: {
-        toolbar: {
-            show: false
-        },
-        zoom: {
-            enabled: false
-        },
-        foreColor: theme.colors.gray[500]
+      toolbar: {
+        show: false,
+      },
+      zoom: {
+        enabled: false,
+      },
+      foreColor: theme.colors.gray[500],
     },
     grid: {
-        show: false
+      show: false,
     },
     dataLabels: {
-        enabled: false
+      enabled: false,
     },
     tooltip: {
-        enabled: false
+      enabled: false,
     },
     xaxis: {
-        type: 'datetime',
-        axisBorder: {
-            color: theme.colors.gray[600]
-        },
-        axisTicks: {
-            color: theme.colors.gray[600]
-        },
-        categories: [
-            '2021-07-2T000:00:00.000Z',
-            '2021-07-11T000:00:00.000Z',
-            '2021-07-17T000:00:00.000Z',
-            '2021-07-20T000:00:00.000Z',
-            '2021-07-27T000:00:00.000Z',
-            '2021-07-30T000:00:00.000Z',
-            '2021-08-2T000:00:00.000Z',
-        ]
+      type: "datetime",
+      axisBorder: {
+        color: theme.colors.gray[600],
+      },
+      axisTicks: {
+        color: theme.colors.gray[600],
+      },
+      categories: vendas.map((d) =>
+        moment(d["dataVenda"]).format("YYYY-MM-DD")
+      ),
     },
     fill: {
-        opacity: 0.3,
-        type: 'gradient', 
-        gradient: {
-            shade: 'dart', 
-            opacityFrom: 0.7,
-            opacityTo: 0.3
-        }
-    }
-};
+      opacity: 0.3,
+      type: "gradient",
+      gradient: {
+        shade: "dart",
+        opacityFrom: 0.7,
+        opacityTo: 0.3,
+      },
+    },
+  };
 
-const series = [
+  const series = [
     {
-        name: 'series1', data: [31, 120, 10, 28, 51, 18, 109]
-    }
-];
+      name: "vendas",
+      data: vendas.map((c) => c["count"]),
+    },
+  ];
 
-export function ChartLineDashboard() {
-    return (
-        <Chart options={options} series={series} type="area" height={160}/>
-    )
+  return <Chart options={options} series={series} type="area" height={160} />;
 }
--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -11,9 +11,24 @@ import { ChartBarDashboard } from "../../components/ChartBar";
 import { ChartLineDashboard } from "../../components/ChartLine";
 import { Sidebar } from "../../components/Sidebar";
 import { withSSRAuth } from "../../utils/withSSRAuth";
-import { PageNotFound } from "../../fragments/404/404";
+import { useEffect, useState } from "react";
+import { api } from "../../services/apiClient";
+import { LoadPage } from "../../../instrumented/components/Load";
 
 export default function Dashboard() {
+  const [data, setData] = useState<any>();
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    async function fetchData() {
+      setIsLoading(true);
+      const response = await api.get("/dashboard");
+      setData(response.data.found);
+      setIsLoading(false);
+    }
+    fetchData();
+  }, []);
+
   return (
     <>
       <Head>
@@ -21,84 +36,84 @@ export default function Dashboard() {
       </Head>
 
       <Sidebar>
-        <PageNotFound />
+        <LoadPage active={isLoading}>
+          <SimpleGrid columns={2} spacing={10} flex="1">
+            <Box
+              p={["6", "8"]}
+              borderRadius={8}
+              border="1px"
+              bg="gray.700"
+              color="white"
+              pb="4"
+            >
+              <Heading as="h2" title="estoque" size="md">
+                Estoque
+              </Heading>
+
+              <Divider mb="4" />
+              <SimpleGrid align="end" spacing={4}>
+                <Stack>
+                  <Text>Itens com estoque baixo</Text>
+                  <Text>{data?.estoqueMinimo[0].count ?? 0}</Text>
+                </Stack>
+                <Stack>
+                  <Text>Itens cadastrados</Text>
+                  <Text>{data?.totalProdutos[0].count ?? 0}</Text>
+                </Stack>
+                <Stack>
+                  <Text>Itens em promoção</Text>
+                  <Text>5</Text>
+                </Stack>
+              </SimpleGrid>
+            </Box>
+
+            <Box
+              p={["6", "8"]}
+              borderRadius={8}
+              border="1px"
+              bg="blue.300"
+              color="white"
+              pb="4"
+            >
+              <Heading as="h2" title="vendas" size="md">
+                Vendas
+              </Heading>
+
+              <Divider mb="4" />
+              <SimpleGrid align="end" spacing={4}>
+                <Stack>
+                  <Text>Hoje</Text>
+                  <Text>{data?.vendasHoje[0].cnt ?? 0}</Text>
+                </Stack>
+                <Stack>
+                  <Text>Mês atual</Text>
+                  <Text>{data?.vendasMesAtual[0].cnt ?? 0}</Text>
+                </Stack>
+                <Stack>
+                  <Text>Mês anterior</Text>
+                  <Text>{data?.vendasMesAnterior[0].cnt ?? 0}</Text>
+                </Stack>
+              </SimpleGrid>
+            </Box>
+
+            {data?.produtosMaisVendidos && (
+              <Box p={["6", "8"]} borderRadius={8} pb="4">
+                <Text fontSize="lg" mb="4">
+                  Produtos mais vendidos
+                </Text>
+                <ChartBarDashboard produtos={data?.produtosMaisVendidos} />
+              </Box>
+            )}
+
+            <Box p={["6", "8"]} borderRadius={8} pb="4">
+              <Text fontSize="lg" mb="4">
+                Evolução das vendas no mês atual
+              </Text>
+              <ChartLineDashboard vendas={data?.evolucaoVendaMes} />
+            </Box>
+          </SimpleGrid>
+        </LoadPage>
       </Sidebar>
-
-      {/* <Sidebar>
-        <SimpleGrid columns={2} spacing={10} flex="1">
-          <Box
-            p={["6", "8"]}
-            borderRadius={8}
-            border="1px"
-            bg="gray.700"
-            color="white"
-            pb="4"
-          >
-            <Heading as="h2" title="estoque" size="md">
-              Estoque
-            </Heading>
-
-            <Divider mb="4" />
-            <SimpleGrid align="end" spacing={4}>
-              <Stack>
-                <Text>Itens com estoque baixo</Text>
-                <Text>0</Text>
-              </Stack>
-              <Stack>
-                <Text>Itens cadastrados</Text>
-                <Text>30</Text>
-              </Stack>
-              <Stack>
-                <Text>Itens em promoção</Text>
-                <Text>5</Text>
-              </Stack>
-            </SimpleGrid>
-          </Box>
-
-          <Box
-            p={["6", "8"]}
-            borderRadius={8}
-            border="1px"
-            bg="blue.300"
-            color="white"
-            pb="4"
-          >
-            <Heading as="h2" title="vendas" size="md">
-              Vendas
-            </Heading>
-
-            <Divider mb="4" />
-            <SimpleGrid align="end" spacing={4}>
-              <Stack>
-                <Text>Hoje</Text>
-                <Text>0</Text>
-              </Stack>
-              <Stack>
-                <Text>Julho</Text>
-                <Text>30</Text>
-              </Stack>
-              <Stack>
-                <Text>Agosto</Text>
-                <Text>5</Text>
-              </Stack>
-            </SimpleGrid>
-          </Box>
-
-          <Box p={["6", "8"]} borderRadius={8} pb="4">
-            <Text fontSize="lg" mb="4">
-              Produtos mais vendidos
-            </Text>
-            <ChartBarDashboard />
-          </Box>
-
-          <Box p={["6", "8"]} borderRadius={8} pb="4">
-            <Text fontSize="lg" mb="4">
-              Evolução das vendas
-            </Text>
-            <ChartLineDashboard />
-          </Box>
-        </SimpleGrid>
-      </Sidebar> */}
     </>
   );
 }
